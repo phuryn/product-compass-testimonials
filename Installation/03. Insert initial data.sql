5a9661073ffca6320619ca187d630abf7212f5fa
Pawel Huryn <pawel.huryn@gmail.com>
2025-01-20 09:41:26 +0100

    Update 03. Insert initial data.sql

--- Installation/03. Insert initial data.sql
+++ Installation/03. Insert initial data.sql
@@ -5,7 +5,8 @@ values
     ('header_subtitle', 'Hi there! I would be thrilled if you could take a moment to leave me a testimonial.'),
     ('primary_color', '#1f883d'),
     ('footer_text', 'My newsletter'),
-    ('footer_url', 'https://www.google.com');
+    ('footer_url', 'https://www.google.com'),
+    ('show_tags_on_index', 'true');
 
 -- Insert tags (products to recommend)
 insert into public.tags (name)
